0bfd54b6e6440d6d04aa0cec336622b4d7ed0b89
Dylan Smith <dylan_smartguy@hotmail.com>
2017-08-04 15:16:18 -0700

    rendered chessboard with correct styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import '../stylesheets/board.css';
+
+const Square = (props) => {
+  const style = {
+    display: 'inline-block',
+    width: '75px',
+    height: '75px',
+    marginBottom: '-5px'
+  };
+  return (
+    <div style={style}></div>
+  );
+}
 
 class Board extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      board: []
+    };
+  }
   render () {
+
+    for (let i = 0; i < 64; i++) {
+      this.state.board.push(<Square />);
+    }
+
     return (
-      
+      <div className='board'>
+        {this.state.board}
+      </div>
     );
   }
 }
